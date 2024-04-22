50b189267d06e8c41f2f70d626a853e5e7aebd96
micphone <2082183611@qq.com>
2024-04-22 19:22:36 +0800

    Initial commit

---
+++ src/main/java/com/food/additives/FoodAdditives.java
@@ -0,0 +1,28 @@
+package com.food.additives;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+
+@Mod(FoodAdditives.MOD_ID)
+public class FoodAdditives {
+    public static final String MOD_ID = "foodadditives";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public FoodAdditives(IEventBus modEventBus) {
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+}
